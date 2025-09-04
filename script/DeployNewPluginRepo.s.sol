28951c0a5b64ca681337ab365c30e657e37717aa
Giorgi Lagidze <giorgilagidze0920@gmail.com>
2025-09-04 17:01:01 +0400

    test

--- script/DeployNewPluginRepo.s.sol
+++ script/DeployNewPluginRepo.s.sol
@@ -4,14 +4,6 @@ pragma solidity ^0.8.17;
 import {Script, console} from "forge-std/Script.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 
-import {DAO} from "@aragon/osx/src/core/dao/DAO.sol";
-import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
-import {LockToVotePluginSetup} from "../src/setup/LockToVotePluginSetup.sol";
-import {PluginRepoFactory} from "@aragon/osx/src/framework/plugin/repo/PluginRepoFactory.sol";
-import {PluginRepo} from "@aragon/osx/src/framework/plugin/repo/PluginRepo.sol";
-import {PluginSetupProcessor} from "@aragon/osx/src/framework/plugin/setup/PluginSetupProcessor.sol";
-import {TestToken} from "../test/mocks/TestToken.sol";
-
 contract Nice {}
 
 contract DeployNewPluginRepoScript is Script {
